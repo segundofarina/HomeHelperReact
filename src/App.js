6521e349668cfeeb931b00148b169e4025b9cd76
tinchovictory <martin@victory.com.ar>
2018-11-09 16:20:50 -0300

    Adding routing to project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import styles from './App.module.css'
 import Layout from './hoc/Layout/Layout'
 import Home from './containers/home/Home'
@@ -7,12 +8,14 @@ import Home from './containers/home/Home'
 class App extends Component {
   render() {
     return (
-      <div className={styles.app}>
-        <Layout>
-          <Home/>
-        </Layout>
-      </div>
-    );
+      <BrowserRouter>
+        <div className={styles.app}>
+          <Layout>
+            <Route path="/" exact component={Home} />
+          </Layout>
+        </div>
+      </BrowserRouter>
+   );
   }
 }
 
